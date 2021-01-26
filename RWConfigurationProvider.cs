2e3855d453688b9873cb2acf171d32c1656dd80b
Vojtěch Lukeš <vojta.luk@seznam.cz>
2021-01-26 23:59:04 +0100

    Přidejte soubory projektu.

---
+++ RWConfigurationProvider.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RWConfiguration
+{
+    class RWConfigurationProvider : IConfigurationProvider
+    {
+        private string path;
+        private Dictionary<string, string> pairs;
+
+        public RWConfigurationProvider(string path)
+        {
+            this.path = path;
+        }
+
+        public IEnumerable<string> GetChildKeys(IEnumerable<string> earlierKeys, string parentPath)
+        {
+            return Enumerable.Empty<string>();
+        }
+
+        public IChangeToken GetReloadToken()
+        {
+            return NullChangeToken.Singleton;
+        }
+
+        public void Load()
+        {
+            pairs = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(path));
+        }
+
+        public void Set(string key, string value)
+        {
+            pairs[key] = value;
+            File.WriteAllText(path, JsonConvert.SerializeObject(pairs, Formatting.Indented));
+        }
+
+        public bool TryGet(string key, out string value)
+        {
+            return pairs.TryGetValue(key, out value);
+        }
+    }
+}
---
+++ RWConfigurationSource.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace RWConfiguration
+{
+    public class RWConfigurationSource : IConfigurationSource
+    {
+        private string path;
+        public RWConfigurationSource(string path)
+        {
+            this.path = path;
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new RWConfigurationProvider(path);
+        }
+    }
+}
